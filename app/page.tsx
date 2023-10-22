0ea634e3a11a127b98721a541dc29e90cd99ca47
isaacthajunior <isaacjunez669@gmail.com>
2023-10-22 17:30:54 +0100

    styling and nav added starting labelling and deleted

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,35 @@
 "use client";
 
-import Image from "next/image";
-import { useState, useEffect } from "react";
+import { Typography } from "@mui/material";
+import React from "react";
 import styles from "./page.module.css";
 import axios from "axios";
-import { CldUploadWidget, CldUploadButton, CldImage } from "next-cloudinary";
+import { useState, useEffect } from "react";
 
-declare global {
-  var cloudinary: any;
-}
-
-interface UploadResult {
-  info: {
-    files: [];
-    public_id: string;
-    secure_url: string;
-  };
-}
-
-const uploadPreset = "sj9mklh4";
-
-export default function Home() {
+export default function page() {
   const [uploadedImages, setUploadedImages] = useState([] as string[]);
 
   useEffect(() => {
     // Create a function to send all images to the database
     const sendImagesToDatabase = async () => {
       try {
-        await axios.post("/api/images", { Images: uploadedImages });
-        console.log("Images saved to the database!");
+        const request = await axios.get("/api/images");
+        const allImages = request.data.map((image: any) => image.Images).flat();
+        console.log(allImages);
+        setUploadedImages(allImages);
       } catch (error) {
-        console.error("Error saving images to the database:", error);
+        console.error("Error fetching images from the database:", error);
       }
     };
 
     // Check if there are images to send and then call the function
-    if (uploadedImages.length > 0) {
-      sendImagesToDatabase();
-    }
-  }, [uploadedImages]); // This will run whenever uploadedImages change
 
-  const handleUpload = async (result: UploadResult) => {
-    // Get the secure URL from the result and add it to the state array.
-    const mappedData = result?.info?.files.map((file: any) => {
-      return file.uploadInfo.secure_url;
-    });
-
-    setUploadedImages(mappedData);
-  };
-
+    sendImagesToDatabase();
+  }, []); // This will run whenever uploadedImages change
   return (
-    <main className={styles.main}>
-      <CldUploadButton
-        onQueuesEnd={handleUpload}
-        uploadPreset={uploadPreset}
-        options={{ maxFiles: 3, clientAllowedFormats: ["png", "jpeg"] }}
-      />
-
-      <div className={styles.imagesContainer}>
-        {uploadedImages.map((url, index) => (
-          <div key={index}>
-            <Image src={url} alt={`Image ${index}`} width={300} height={200} />
-          </div>
-        ))}
-      </div>
-    </main>
+    <>
+      {uploadedImages.length}
+      <Typography>All Images</Typography>
+    </>
   );
 }
